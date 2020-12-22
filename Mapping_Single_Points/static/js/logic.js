1b5849e2899581c2faea483ed265440f3dd840bf
EHuiMartinez <69170347+EHuiMartinez@users.noreply.github.com>
2020-12-22 15:12:00 -0500

    update

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -5,11 +5,10 @@ console.log("working");
 let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 
 //  Add a marker to the map for Los Angeles, California.
-//let marker = L.marker([34.0522, -118.2437]).addTo(map);
 L.circleMarker([34.0522, -118.2437], {
-    radius:300,
+    radius: 300,
     color: "black",
-    fillColor: "yellow"
+    fillColor: '#ffffa1'
 }).addTo(map);
 
 // We create the tile layer that will be the background of our map.
